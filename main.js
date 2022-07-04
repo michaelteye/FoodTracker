7857d06a8e5e675a861dd985d2bf0cf01b2b219a
michaelteye <michaelteyegyemah@gmail.com>
2022-07-04 11:01:06 +0000

    a simple foodtracker

---
+++ main.js
@@ -0,0 +1,67 @@
+// Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value.toLowerCase()
+  const url = `https://world.openfoodfacts.org/api/v0/product/${choice}.json`
+  if(choice.length !==12){
+    alert(`Please ensure that barcode is 12 characters`)
+    return ;
+  }
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if(data.status === 1){
+             // call additional stuffs
+             const item = new ProductInfo(data.product)
+             item.showInfo()
+             item.listIngredients()
+
+        }else if(data.status === 0){
+            alert(`Product ${choice} not found try another one`)
+        }
+       
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+class ProductInfo{
+    constructor(productData){
+        this.name = productData.product_name
+        this.ingredients = productData.ingredients
+        this.image = productData.image_url 
+    }
+    showInfo(){
+        document.getElementById('product-img').src = this.image
+        document.getElementById('product-name').innerText = this.name
+    }
+    listIngredients(){
+        let tableRef = document.getElementById('ingredient-table')
+        for(let i = 1; i< tableRef.rows.length;){
+            tableRef.deleteRow(i)
+        }
+        for(let key in this.ingredients){
+            let newRow = tableRef.insertRow(-1)
+            let newICell = newRow.insertCell(0)
+            let newVCell = newRow.insertCell(1)
+            let newIText = document.createTextNode(this.ingredients[key].text)
+            let vegStatus = this.ingredients[key].vegetarian
+            let newVText = document.createTextNode(vegStatus)
+            newICell.appendChild(newIText)
+            newVCell.appendChild(newVText)
+            if(vegStatus === 'no'){
+                newVCell.classList.add('non-veg-item')
+            }else if(vegStatus === 'unknown'|| vegStatus === 'maybe')
+            {
+                newVCell.classList.add('unknown-maybe-item')
+            }
+
+
+        }
+    }
+}
+//011110038364
+
